075102f1c82b39ec73f797058807bc57476dda13
DrKondor <snakey@deltadevils.net>
2015-08-13 02:57:05 -0600

    Update BacktrackTransfer.sh

--- BacktrackTransfer.sh
+++ BacktrackTransfer.sh
@@ -2,6 +2,8 @@
 
 #Jesus Gawd this script is rough. Barely even functional at the mo. But it works! Anyway, I've got a session (lets be honest) so I'll throw it up on the Github and fix it later. 
 
+#Update 8-12-2015 Something's changed in debian jessie and this script is no longer working. I had the wild, late night idea to create and use a symlink to do the syncup, but I'm unable to code in this half-sleep state.  
+
 foldername=$(date +%Y%m%d)
 
 read -p "Where do you want to save your tracks?" DIRECTORY
